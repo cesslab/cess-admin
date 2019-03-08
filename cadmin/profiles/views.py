9eedf89e8259f4801ffb8c7082699b2f920bfc77
cesslab <anwarruff@gmail.com>
2019-03-08 10:55:42 -0500

    Added project model

--- cadmin/profiles/views.py
+++ cadmin/profiles/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+from .forms import CustomUserCreationForm
 
 
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('projects_home')
 
     form = CustomUserCreationForm()
