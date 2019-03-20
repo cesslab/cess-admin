aeb88c2b2d3507560d8f730536e42a1247c7e996
cesslab <anwarruff@gmail.com>
2019-03-20 15:26:49 -0400

    Created "add project" form and view

--- cadmin/projects/views.py
+++ cadmin/projects/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ProjectForm
+from .models import Project
 
 
 @login_required
 def projects(request):
-    return render(request, 'projects.html')
+    project_list = Project.objects.all()
+    return render(request, 'projects.html', {'projects': project_list})
 
 
+@login_required
 def add_project(request):
     if request.method == 'POST':
-        pass
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.save()
+            project.users.add(request.user)
+            project.save()
+            return redirect('projects_home')
     form = ProjectForm()
     return render(request, 'add_project.html', {'form': form})
 
