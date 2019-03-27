134c478fed2a9a734014ec8d2a88db8b201c0a55
cesslab <anwarruff@gmail.com>
2019-03-27 16:01:23 -0400

    Added project to django admin

--- cadmin/projects/admin.py
+++ cadmin/projects/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import User, Project
 
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    filter_horizontal = ('collaborators', )
+
+
 admin.site.register(User)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
