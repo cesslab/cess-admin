938a94fae5aa901c1b5f7e16295dbb0a352c86e4
cesslab <anwarruff@gmail.com>
2019-03-27 14:05:28 -0400

    Added projects to django admin

--- cadmin/projects/admin.py
+++ cadmin/projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import User, Project
 
 admin.site.register(User)
+admin.site.register(Project)
