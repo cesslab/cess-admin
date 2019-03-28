71b2b71769b8f4996885f75e88080e01369286a8
cesslab <anwarruff@gmail.com>
2019-03-28 16:04:05 -0400

    Added approval field and related name to projects model

--- cadmin/projects/models.py
+++ cadmin/projects/models.py
@@ -4,7 +4,8 @@ from profiles.models import User
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
-    collaborators = models.ManyToManyField(User)
+    approved = models.BooleanField(default=False)
+    collaborators = models.ManyToManyField(User, related_name='projects')
 
     def __str__(self):
         return self.name
