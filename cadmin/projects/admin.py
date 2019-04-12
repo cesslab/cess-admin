b281603824d2df034659ad2cdaab537ec8d246d7
cesslab <anwarruff@gmail.com>
2019-04-12 14:25:09 -0400

    Added updated many to many user fields

--- cadmin/projects/admin.py
+++ cadmin/projects/admin.py
@@ -6,7 +6,7 @@ from .models import User, Project
 
 class ProjectAdmin(admin.ModelAdmin):
     model = Project
-    filter_horizontal = ('collaborators', )
+    filter_horizontal = ('primary_investigators', 'research_assistants',)
 
 
 admin.site.register(User)
