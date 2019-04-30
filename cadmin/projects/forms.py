5c5f2d5c402bc134b6b8724a51120e799a942205
cesslab <anwarruff@gmail.com>
2019-04-30 14:18:27 -0400

    added irb admin fields

--- cadmin/projects/forms.py
+++ cadmin/projects/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from .models import Project, User, FileItem
 from django_select2.forms import Select2MultipleWidget
+from crispy_forms.helper import FormHelper
 
 
 class ProjectForm(forms.ModelForm):
+    irb_approval_method = forms.ChoiceField(label='IRB approval method', choices=Project.IRB_APPROVAL_CHOICES)
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
         super(ProjectForm, self).__init__(*args, **kwargs)
@@ -13,17 +15,17 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = [
-            'name', 'description', 'primary_investigators', 'research_assistants',
-            # 'has_irb_cert', 'external_irb_approval', 'protocol', 'is_no_deception', 'has_grant_funding',
-            # 'grant_agency', 'grant_funds_released', 'alt_source_funding'
+            'name', 'description', 'primary_investigators', 'research_assistants', 'irb_approval_method', 'approved'
               ]
         widgets = {'primary_investigators': Select2MultipleWidget(), 'research_assistants': Select2MultipleWidget}
 
         def __init__(self, *args, **kwargs):
             user = kwargs.pop('user')
             super(Project, self).__init__(*args, **kwargs)
+            # Remove admin fields if the user is a researcher
             if not user.is_irbadmin:
                 del self.fields['approved']
+                del self.fields['irb_approval_method']
 
     @staticmethod
     def label_from_instance(obj: User):
@@ -34,6 +36,5 @@ class FileItemForm(forms.ModelForm):
     class Meta:
         model = FileItem
         fields = ['file']
-
 
 
